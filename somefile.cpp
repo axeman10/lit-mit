fdd2cc84ffbe24755f44bb253deb7e3beabcf8b6
axe leo <axemanakacaptain@gmail.com>
2017-10-04 13:04:35 +0300

    added vector

--- somefile.cpp
+++ somefile.cpp
@@ -10,6 +10,9 @@ using std::endl;
 int main(void)
 {
 	cout << "hello world" << endl;
+	std::vector<int> hec = {1,2,1,2,4,7,55,86,10};
 	
+	for(int& some : hec)
+		cout << some << endl;
 	return 0;
 }
