1057e85093ab905dba812be256ea9d8ac132cbb1
axe leo <axemanakacaptain@gmail.com>
2017-10-04 11:29:39 +0300

    added cpp file

---
+++ somefile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+	cout << "hello world" << endl;
+	
+	return 0;
+}
